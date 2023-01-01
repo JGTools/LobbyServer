4a8b79cb60281d6a5d8d0a100f1c5a9130e46d12
d <erikp.bratthall@gmail.com>
2023-01-02 00:32:44 +0100

    prep for publish

--- README.md
+++ README.md
@@ -31,7 +31,26 @@ import LobbyServer from "@jgtools/lobbyserver";
 ## Usage
 
 ```typescript
-import LobbyServer from "@jgtools/lobbyserver";
+import LobbyServer, { LobbyUser_I } from "@jgtools/lobbyserver";
+
+interface LobbyPlayer_I extends LobbyUser_I {
+  color: number;
+}
+
+const lobby = new LobbyServer("lobbyid", { maxPlayers: 8 });
+
+const user: LobbyPlayer_I = {
+  id: "123",
+  name: "Bob",
+  color: 0xff0000,
+};
+
+lobby.addUser(user);
+console.log(lobby.getLobby());
+lobby.setMeta(user, { maxPlayers: 9 });
+console.log(lobby.getLobby());
+lobby.removeUser(user.id);
+console.log(lobby.getLobby());
 ```
 
 ## License
--- examples/test.ts
+++ examples/test.ts
@@ -1,21 +1,20 @@
 import LobbyServer, { LobbyUser_I } from "../src";
 
-const lobby = new LobbyServer("id", { maxPlayers: 8 });
-
 interface LobbyPlayer_I extends LobbyUser_I {
     color: number;
 }
 
+const lobby = new LobbyServer("lobbyid", { maxPlayers: 8 });
+
 const user: LobbyPlayer_I = {
-    id: "asdf",
+    id: "123",
     name: "Bob",
-    color: 0xff0000
+    color: 0xff0000,
 };
 
 lobby.addUser(user);
 console.log(lobby.getLobby());
-lobby.removeUser(user.id);
-console.log(lobby.getLobby());
-lobby.addUser(user);
 lobby.setMeta(user, { maxPlayers: 9 });
+console.log(lobby.getLobby());
+lobby.removeUser(user.id);
 console.log(lobby.getLobby());
